b0b793c67216b06a42ba624f3a309692d0563ba7
Rémi San <remi.san@gmail.com>
2020-12-04 17:11:08 +0100

    feat: add glove animation

--- scenes/elements/enemies/BoxingGlove.cs
+++ scenes/elements/enemies/BoxingGlove.cs
@@ -3,22 +3,39 @@ using System;
 
 public class BoxingGlove : Area2D
 {
+    private const float MaxSpeed = 300f;
+
     [Export] private Vector2 Direction = Vector2.Left;
     [Export] private float Strength = 450f;
-    [Export] private float Speed = 300f;
+    [Export] private float Speed = 0f;
 
     private DodgeDetector DodgeDetector;
+    private AnimationPlayer AnimationPlayer;
 
     public EnemyProperties.Type Type = EnemyProperties.Type.BoxingGlove;
 
     public override void _Ready()
     {
         this.DodgeDetector = this.GetNode<DodgeDetector>("DodgeDetector");
+        this.AnimationPlayer = this.GetNode<AnimationPlayer>("AnimationPlayer");
+
+        this.Appear();
     }
 
     public override void _PhysicsProcess(float delta)
     {
         this.Position += this.Direction * new Vector2(this.Speed * delta, 0);
+    }
+
+    public void Appear()
+    {
+        this.AnimationPlayer.Play("Appear");
+    }
+
+    public void Launch()
+    {
+        this.Speed = MaxSpeed;
+        this.AnimationPlayer.Play("Idle");
     }
 
     private void Destroy()
--- scenes/levels/TestScene.cs
+++ scenes/levels/TestScene.cs
@@ -3,6 +3,11 @@ using System;
 
 public class TestScene : Node2D
 {
+    public override void _Ready()
+    {
+        this.GetNode<Character>("Character").WakeUp();
+    }
+
     private void _on_Character_Punched()
     {
         GD.Print("punched");
